99ba41d37f27fca6fdc6c65bbdb9e367e24325d1
aqt <git@raz.sexy>
2017-10-01 17:05:53 +0200

    Always try to open popup, display error if it fails

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-	let url = tabs[0].url
+	let url = tabs[0].url;
 
-	if (url.substr(0, 4) === "http") {
+	try {
 		chrome.windows.create({ url, type: "popup" });
+		window.close();
+	} catch(e) {
+		output.innerHTML = `<strong>Cannot open a popup for this URL</strong> <hr> <code>${e}</code>`;
 	}
-
-	window.close();
 });
