049fe23373dc7ed472182d254b821f40f810e4eb
aqt <git@raz.sexy>
2018-03-25 14:17:47 +0200

    Add support for localized titles in settings

--- content/_shared/i18n.js
+++ content/_shared/i18n.js
@@ -5,6 +5,13 @@ function DOMLoaded() {
 	elements.forEach(el => {
 		el.textContent = browser.i18n.getMessage(el.getAttribute(attr));
 	});
+
+	attr = "data-i18n-title";
+	elements = document.querySelectorAll(`[${attr}]`);
+
+	elements.forEach(el => {
+		el.title = browser.i18n.getMessage(el.getAttribute(attr));
+	});
 }
 
 document.addEventListener("DOMContentLoaded", DOMLoaded);
