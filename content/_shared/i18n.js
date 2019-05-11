e50365d0fbeb6832f80eea7d2c096219fca8042b
aqt <aqt@users.noreply.github.com>
2019-05-11 19:04:26 +0200

    Show placeholder text for i18n when opening just the html files

--- content/_shared/i18n.js
+++ content/_shared/i18n.js
@@ -2,15 +2,19 @@ function DOMLoaded() {
 	let attr = "data-i18n-message";
 	let elements = document.querySelectorAll(`[${attr}]`);
 
+	let usePlaceholder = typeof browser === "undefined";
+
 	elements.forEach(el => {
-		el.textContent = browser.i18n.getMessage(el.getAttribute(attr));
+		let text = el.getAttribute(attr);
+		el.textContent = usePlaceholder ? "$"+text : browser.i18n.getMessage(text);
 	});
 
 	attr = "data-i18n-title";
 	elements = document.querySelectorAll(`[${attr}]`);
 
 	elements.forEach(el => {
-		el.title = browser.i18n.getMessage(el.getAttribute(attr));
+		let text = el.getAttribute(attr);
+		el.title = usePlaceholder ? "$"+text : browser.i18n.getMessage(text);
 	});
 }
 
